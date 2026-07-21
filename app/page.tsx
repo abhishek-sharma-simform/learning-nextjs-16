bb507b8776f54849e35cd8b62643924a5babda37
abhishek_simform <abhishek.sharma@simformsolutions.com>
2026-07-21 13:07:42 +0530

    feat : adding styling.

--- app/page.tsx
+++ app/page.tsx
@@ -6,5 +6,9 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  return <h2>Hello</h2>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+    </main>
+  );
 }
